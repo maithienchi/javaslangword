ac91cf612ed078efa6e0f6f308c8f96988584034
Chi Mai Thien <mtchi2019@gmail.com>
2021-05-03 15:08:16 +0700

    Update slangmanager
    add TimKiemTheoDefition ,
    editSlangWord

--- slangword/src/slangword/slangmanager.java
+++ slangword/src/slangword/slangmanager.java
@@ -53,5 +53,61 @@ public class slangmanager {
             System.out.println("key not found, please input another key!!!");
         }
     }
+    public List<String> TimKiemTheoDefition(String str){
+        int dem = 0;
+        Date date = new Date();
+        SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+//      System.out.println(formatter.format(date));0
+        List<String> listWord = new ArrayList<>();
+        for(slang p: slangList){
+//            for(int i=0;i<str.length();i++){
+//                if(p.getKey().indexOf(str.charAt(i))>=0){
+//                    
+//                }
+//            }
+            if(p.getKey().indexOf(str)>=0){
+//                System.out.println("key: "+p.getKey()+"\n"+ "mean: "+p.getMean());
+                listWord.add(p.getKey());
+                dem++;
+            }    
+        }
+        search_History_List.add(formatter.format(date)+ " Tim kiem key Theo Defition: " + str);
+        slangwordDao.write_search_history(search_History_List);
+        if(dem==0){
+            System.out.println("key not found, please input another key!!!");
+        }
+        return listWord;
+    }
+    public void editSlangWord(){
+    System.out.println("Nhap vao key: ");
+    String key = scanner.nextLine();
+    String listWord = "";
+    for(int i=0;i<slangList.size();i++){
+        if(slangList.get(i).getKey().compareTo(key)==0){
+            System.out.println("key: "+slangList.get(i).getKey()+"\n"+ "mean: "+slangList.get(i).getMean());
+            System.out.println("Nhap vao mean can thay doi: ");
+            System.out.println("Nhap vao so luong mean: ");
+            int soluong = Integer.parseInt(scanner.nextLine());
+            for (int j=0;j<soluong;j++){
+                System.out.println("Nhap vao mean thu "+((int)(j+1))+": ");
+                if(j==0){
+                    listWord += scanner.nextLine();
+                }
+                else{
+                    listWord += " | "+scanner.nextLine();
+                }
+                
+            }
+            slangList.get(i).setMean(listWord);
+            slangwordDao.emtyfile("edit_history_path.txt");
+            slangwordDao.writeEditSlang(slangList);
+            System.out.println("change done");
+        }
+    }
+    if(key==""){
+        System.out.println("key not found, please input another key!!!");
+    }
+    System.out.println("key: "+key+"\n"+ "mean: "+listWord);
+    }
     
 }
