584081107f79b737819a0f2008e6fb3a951ed16d
Chi Mai Thien <mtchi2019@gmail.com>
2021-05-03 14:54:15 +0700

    update slangwordDao
    add read_search_history_path write writeEditSlang_search_history

--- slangword/src/slangword/slangwordDao.java
+++ slangword/src/slangword/slangwordDao.java
@@ -28,14 +28,14 @@ public class slangwordDao {
         List<slang> slangList = new ArrayList<slang>();
         try {
             File f = new File(slang_path);
-            if(f.exists() && !f.isDirectory()) { 
+            if(f.exists() && !f.isDirectory()) { //Kiem tra file ton tai
             }
             else{
                 System.out.println("Nhap vao lien ket den file slang.txt: ");
                 slang_path= scanner.nextLine();
             }
-            FileInputStream fstream = new FileInputStream(slang_path); 
-            BufferedReader br = new BufferedReader(new InputStreamReader(fstream));// tham khao nguon https://stackoverflow.com/questions/14657997/reading-from-file-and-splitting-the-data-in-java
+            FileInputStream fstream = new FileInputStream(slang_path);
+            BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
             String strLine;
             while ((strLine = br.readLine()) != null) {
                 String[] splitOut = strLine.split("`");
@@ -51,7 +51,62 @@ public class slangwordDao {
         return slangList;
     }
     
-    
+    public List<String> read_search_history_path(){
+        List<String> search_history = new ArrayList<String>();
+        try {
+            File f = new File(search_history_path);
+            if(f.exists() && !f.isDirectory()) { //Kiem tra file ton tai
+            }
+            else{
+                System.out.println("Nhap vao lien ket den file search_history_path.txt: ");
+                search_history_path= scanner.nextLine();
+            }
+            FileInputStream fstream = new FileInputStream(search_history_path);
+            BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
+//            File file = new File("search_history_path.txt");
+//            BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file), StandardCharsets.ISO_8859_1));
+            String strLine;
+            while ((strLine = br.readLine()) != null) {
+                String[] splitOut = strLine.split("\n");
+                if (splitOut.length == 1) {
+                    search_history.add(new String(splitOut[0]));
+                } else {
+                    System.out.println("Invalid class: " + strLine);
+                }
+            }
+        } catch (Exception e) {
+            System.err.println("Error: " + e.getMessage());
+        }
+        return search_history;
+    }
+    public void write_search_history(List<String> list_search){
+//        FileOutputStream fos = null;
+//        ObjectOutputStream oos = null;
+//        try {
+//            fos = new FileOutputStream(new File("search_history_path.txt"));
+//            oos = new ObjectOutputStream(fos);
+//            oos.writeObject(list_search);
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        }catch (IOException e){
+//            e.printStackTrace();            
+//        } finally {
+//            closeStream(fos);
+//            closeStream(oos);
+//        }
+
+        try {
+            FileOutputStream fstream = new FileOutputStream(search_history_path);
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fstream));
+            for (String s : list_search) {
+            bw.write(s);
+            bw.newLine();
+        }
+        bw.close();
+        } catch (Exception e) {
+            System.err.println("Error: " + e.getMessage());
+        }
+    }
     public void write(List<slang> slangList){
 //        FileOutputStream fos = null;
 //        ObjectOutputStream oos = null;
@@ -79,6 +134,23 @@ public class slangwordDao {
             System.err.println("Error: " + e.getMessage());
         }
     }
+    
+    public void writeEditSlang(List<slang> slangList){
+        try {
+            FileOutputStream fstream = new FileOutputStream(edit_history_path);
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fstream));
+            for (slang s : slangList) {
+            bw.write(s.getKey()+"`"+s.getMean());
+            bw.newLine();
+        }
+        bw.close();
+        } catch (Exception e) {
+            System.err.println("Error: " + e.getMessage());
+        }
+    }
+    
+    
+   
     
     
     
